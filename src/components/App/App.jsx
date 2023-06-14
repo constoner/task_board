1ab310ea392751e68642bb6054517f5984e90176
constoner <constantin.kalinin@gmail.com>
2023-06-15 01:13:30 +0300

    repairing the footer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -95,6 +95,9 @@ const App = () => {
         <Box
           component="footer"
           sx={{
+            position: "absolute",
+            bottom: "0px",
+            width: "100%",
             boxShadow:
               "0px -2px 4px -1px rgba(0,0,0,0.2),0px -4px 5px 0px rgba(0,0,0,0.14),0px -1px 10px 0px rgba(0,0,0,0.12)",
           }}
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import ReactDOM from "react-dom/client";
 // Components
 import App from "./components/App/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <>
     <App />
