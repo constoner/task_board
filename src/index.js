5512be503fc339a3575660f65e558b166acc183a
constoner <constantin.kalinin@gmail.com>
2023-06-15 23:58:05 +0300

    trying to disable body scroll

--- src/index.js
+++ src/index.js
@@ -7,6 +7,22 @@ import App from "./components/App/App";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
+
+// Disabling body scroll
+const bodyStyle = {
+  position: "fixed",
+  left: "0",
+  top: "0",
+  right: "0",
+  bottom: "0",
+  touchAction: "none",
+  overflow: "hidden",
+  height: "100%",
+  maxHeight: "100%",
+};
+Object.assign(document.body.style, bodyStyle);
+
+// Render App
 root.render(
   <>
     <App />
