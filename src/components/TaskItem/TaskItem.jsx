39f3243141019deca6953f83715f4e75648ab4ce
constoner <constantin.kalinin@gmail.com>
2023-07-29 02:23:50 +0300

    round 2

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -47,7 +47,7 @@ const TaskItem = ({ id, name, cb }) => {
     setDoc(doc(db, "tasks", id), {
       name: taskName.trim(),
     });
-    setTimeout(() => taskName && setEditState(false), 100);
+    setTimeout(() => taskName && setEditState(false), 200);
   };
 
   return (
